df6367b281ad20c374acba01c312c4727692b5a7
HariYenuganti <haripranay07@gmail.com>
2025-12-23 16:40:42 -0500

    feat(state): add pet context, hooks and types

---
+++ src/contexts/pet-context-provider.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { createContext, useState } from 'react';
+import { Pet } from '@/lib/types';
+
+type PetContextProviderProps = {
+  data: Pet[];
+  children: React.ReactNode;
+};
+
+type TPetContext = {
+  pets: Pet[];
+  selectedPetId: string | null;
+  selectedPet: Pet | undefined;
+  numberOfPets: number;
+  handleSelectedPetId: (id: string) => void;
+};
+
+export const PetContext = createContext<TPetContext | null>(null);
+
+export default function PetContextProvider({
+  data,
+  children,
+}: PetContextProviderProps) {
+  // State
+  const [pets, setPets] = useState(data);
+  const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
+
+  //derived state
+  const selectedPet = pets.find((pet) => pet.id === selectedPetId);
+  const numberOfPets = pets.length;
+
+  // event handlers
+  const handleSelectedPetId = (id: string) => {
+    setSelectedPetId(id);
+  };
+
+  return (
+    <PetContext.Provider
+      value={{
+        pets,
+        selectedPetId,
+        selectedPet,
+        numberOfPets,
+        handleSelectedPetId,
+      }}
+    >
+      {children}
+    </PetContext.Provider>
+  );
+}

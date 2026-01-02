ebc64a5333fd0cba8f3fd27adc7d474d7e67f062
HariYenuganti <haripranay07@gmail.com>
2026-01-02 15:32:50 -0500

    feat: implement search functionality

---
+++ src/contexts/search-context-provider.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { createContext, useState } from 'react';
+
+type SearchContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type TSearchContext = {
+  searchQuery: string;
+  handleChangeSearchQuery: (query: string) => void;
+};
+
+export const SearchContext = createContext<TSearchContext | null>(null);
+
+export default function SearchContextProvider({
+  children,
+}: SearchContextProviderProps) {
+  // State
+  const [searchQuery, setSearchQuery] = useState('');
+  //derived state
+
+  // event handlers
+  const handleChangeSearchQuery = (query: string) => {
+    setSearchQuery(query);
+  };
+
+  return (
+    <SearchContext.Provider value={{ searchQuery, handleChangeSearchQuery }}>
+      {children}
+    </SearchContext.Provider>
+  );
+}

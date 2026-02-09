e85839710922dce5d0fe5bdfb624ed95551448c5
HariYenuganti <haripranay07@gmail.com>
2026-02-09 15:58:46 -0500

    refactor: exclude userId from PetEssentials type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,7 @@
 import { Pet } from '@prisma/client';
 export type { Pet };
 
-export type PetEssentials = Omit<Pet, 'id' | 'createdAt' | 'updatedAt'>;
+export type PetEssentials = Omit<
+  Pet,
+  'id' | 'createdAt' | 'updatedAt' | 'userId'
+>;

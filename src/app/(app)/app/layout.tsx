6516b789851479b60adfcb4773b84257f32223eb
HariYenuganti <haripranay07@gmail.com>
2026-02-09 15:10:44 -0500

    feat: protect app routes and integrate user specific data

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -3,15 +3,16 @@ import AppHeader from '@/components/app-header';
 import AppFooter from '@/components/app-footer';
 import PetContextProvider from '@/contexts/pet-context-provider';
 import SearchContextProvider from '@/contexts/search-context-provider';
-import prisma from '@/lib/db';
 import { Toaster } from 'sonner';
+import { checkAuth, getPetsByUserId } from '@/lib/server-utils';
 
 export default async function layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pets = await prisma.pet.findMany();
+  const session = await checkAuth();
+  const pets = await getPetsByUserId(session.user.id);
   return (
     <>
       <BackgroundPattern />

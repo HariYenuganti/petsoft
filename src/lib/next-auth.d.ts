ff80e60e8d3e72fc0256be23b233667f71523430
HariYenuganti <haripranay07@gmail.com>
2026-02-12 12:17:15 -0500

    feat(db): add premium access field and update auth types

--- src/lib/next-auth.d.ts
+++ src/lib/next-auth.d.ts
@@ -3,10 +3,17 @@ import { User } from 'next-auth';
 declare module '@auth/core/jwt' {
   interface JWT {
     userId: string;
+    email: string;
+    hasPremiumAccess: boolean;
   }
 }
 
 declare module 'next-auth' {
+  interface User {
+    hasPremiumAccess: boolean;
+    email: string;
+  }
+
   interface Session {
     user: User & {
       id: string;
